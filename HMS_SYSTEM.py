b5dedc710b5a667fd520806712e323e3659ca650
Anurag Chakraborty <67368703+anurag120799@users.noreply.github.com>
2025-11-30 16:42:06 +0530

    Update HMS_SYSTEM.py

--- HMS_SYSTEM.py
+++ HMS_SYSTEM.py
@@ -2092,4 +2092,5 @@ if __name__ == '__main__':
     print("   Super Admin: admin@medicloud.com / Admin@123\n")
     print("=" * 60 + "\n")
 
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+    app.run(debug=True, host='0.0.0.0')
